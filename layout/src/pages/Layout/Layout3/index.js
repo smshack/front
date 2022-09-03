9c21ebaab70810f011ef9a1c2b5ef3f8f5bd2599
smshack <5432tat@naver.com>
2022-09-03 14:23:32 +0900

    grid를 이용한 레이아웃 잡기

---
+++ layout/src/pages/Layout/Layout3/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Layout3 = () => {
+  return (
+    <div className="Layout3 wrapper">
+      <header className="header">
+        <h1>Grid 방식 사용</h1>
+        header
+      </header>
+      <nav className="nav">nav</nav>
+      <aside className="aside">aside</aside>
+      <section className="section">section</section>
+      <footer className="footer">footer</footer>
+    </div>
+  );
+};
+
+export default Layout3;
--- layout/src/pages/Layout/index.js
+++ layout/src/pages/Layout/index.js
@@ -3,6 +3,7 @@ import {  Route } from "react-router-dom";
 import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom";
 import Layout1 from "./Layout1";
 import Layout2 from "./Layout2";
+import Layout3 from "./Layout3";
 const Layout = ({ match }) => {
   useEffect(() => {
   }, []);
@@ -10,6 +11,7 @@ const Layout = ({ match }) => {
     <div className="Layout">
       <Route path={`${match.path}/layout1`} component={Layout1} />
       <Route path={`${match.path}/layout2`} component={Layout2} />
+      <Route path={`${match.path}/layout3`} component={Layout3} />
     </div>
   );
 };
