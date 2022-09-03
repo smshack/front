db1fb92a2da0eaf087fc1f13c7e5da7c5f67598e
smshack <5432tat@naver.com>
2022-09-04 02:36:38 +0900

    레이아웃3 flex

--- layout/src/pages/Layout/Layout8/index.js
+++ layout/src/pages/Layout/Layout8/index.js
@@ -2,7 +2,15 @@ import React from "react";
 const Layout8 = () => {
   return (
     <div className="Layout8 wrapper">
-      <h1>Layout8</h1>
+      <div className="header">header</div>
+      <div className="nav">nav</div>
+      <div className="main clearfix">
+        <div className="aside">aside</div>
+        <div className="article1">article1</div>
+        <div className="article2">article2</div>
+        <div className="article3">article3</div>
+      </div>
+      <div className="footer">footer</div>
     </div>
   );
 };
