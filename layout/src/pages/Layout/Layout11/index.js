e75576e475e665aebd89ec236dc3acf0dc4be4a1
smshack <5432tat@naver.com>
2022-09-04 14:10:59 +0900

    레이아웃5 float

--- layout/src/pages/Layout/Layout11/index.js
+++ layout/src/pages/Layout/Layout11/index.js
@@ -2,7 +2,23 @@ import React from "react";
 const Layout11 = () => {
   return (
     <div className="Layout11 wrapper">
-      <h1>Layout11</h1>
+      <header>
+        <div className="container"></div>
+      </header>
+      <nav>
+        <div className="container"></div>
+      </nav>
+      <main>
+        <div className="contents container">
+          <div className="cont1"></div>
+          <div className="cont2"></div>
+          <div className="cont3"></div>
+          <div className="cont4"></div>
+        </div>
+      </main>
+      <footer>
+        <div className="container"></div>
+      </footer>
     </div>
   );
 };
