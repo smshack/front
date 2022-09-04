7fc078ce53676f102bd6f4e318833d5ebd4ccb31
smshack <5432tat@naver.com>
2022-09-04 14:27:53 +0900

    레이아웃5 flex

--- layout/src/pages/Layout/Layout12/index.js
+++ layout/src/pages/Layout/Layout12/index.js
@@ -2,7 +2,23 @@ import React from "react";
 const Layout12 = () => {
   return (
     <div className="Layout12 wrapper">
-      <h1>Layout12</h1>
+      <header>
+        <div className="container"></div>
+      </header>
+      <nav>
+        <div className="container"></div>
+      </nav>
+      <main>
+        <div className="contents container clearfix">
+          <div className="cont1"></div>
+          <div className="cont2"></div>
+          <div className="cont3"></div>
+          <div className="cont4"></div>
+        </div>
+      </main>
+      <footer>
+        <div className="container"></div>
+      </footer>
     </div>
   );
 };
