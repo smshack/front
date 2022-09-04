6ec5491fa3aa2ebeda4c92d4bb7dd789f09191de
smshack <5432tat@naver.com>
2022-09-04 14:51:10 +0900

    레이아웃5 grid

--- layout/src/pages/Layout/Layout13/index.js
+++ layout/src/pages/Layout/Layout13/index.js
@@ -2,7 +2,23 @@ import React from "react";
 const Layout13 = () => {
   return (
     <div className="Layout13 wrapper">
-      <h1>Layout13</h1>
+      <header>
+        <div className="container"></div>
+      </header>
+      <nav>
+        <div className="container"></div>
+      </nav>
+      <main>
+        <div className="contents container">
+            <div className="cont1"></div>
+            <div className="cont2"></div>
+            <div className="cont3"></div>
+            <div className="cont4"></div>
+        </div>
+      </main>
+      <footer>
+        <div className="container"></div>
+      </footer>
     </div>
   );
 };
