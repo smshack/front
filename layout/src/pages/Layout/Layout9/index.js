3c70dff00da765fb78b27ce77429b14a93c66702
smshack <5432tat@naver.com>
2022-09-04 12:59:49 +0900

    레이아웃3 grid - 반응형

--- layout/src/pages/Layout/Layout9/index.js
+++ layout/src/pages/Layout/Layout9/index.js
@@ -2,9 +2,18 @@ import React from "react";
 const Layout9 = () => {
   return (
     <div className="Layout9 wrapper">
-      <h1>Layout9</h1>
+      <div className="header">header</div>
+      <div className="nav">nav</div>
+      <div className="main">
+        <div className="aside">aside</div>
+        <div className="article1">article1</div>
+        <div className="article2">article2</div>
+        <div className="article3">article3</div>
+      </div>
+      <div className="footer">footer</div>
     </div>
   );
 };
+
 
 export default Layout9;
