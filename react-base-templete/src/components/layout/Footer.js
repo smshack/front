d423eda8c055d77d5028279f6f41d75352274833
smshack <5432tat@naver.com>
2022-09-06 13:26:20 +0900

    리액트 초기 설정 템플릿

---
+++ react-base-templete/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Route, Link } from "react-router-dom";
+
+const Footer = props => {
+  return (
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  );
+}
+
+Footer.propTypes = {}
+
+export default Footer
